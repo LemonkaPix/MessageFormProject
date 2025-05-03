5d8f659404d06fcbca7626b7f3796d64f387d2c6
LemonkaPix <maciejwrobel029@gmail.com>
2025-05-03 16:43:46 +0200

    Integrate Redux for state management and add messages API service

---
+++ frontend/services/messagesAPI.js
@@ -0,0 +1,35 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const messagesApi = createApi({
+  reducerPath: "messagesApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8080/",
+  }),
+  refetchOnFocus: true,
+  endpoints: (builder) => ({ 
+    // Query to fetch posts
+    getMessages: builder.query({ query: () => "getMessages" }),
+    addMessage: builder.mutation({
+      query: (newMessage) => ({
+        url: "addMessage",
+        method: "POST",
+        body: newMessage,
+      }),
+    }),
+    editMessage: builder.mutation({
+        query: ({ id, ...newMessage }) => ({
+          url: `editMessage/${id}`,
+          method: "PUT",
+          body: newMessage,
+        }),
+      }),
+      deleteMessage: builder.mutation({
+        query: (id) => ({
+          url: `deleteMessage/${id}`,
+          method: "DELETE",
+        }),
+      }),
+    }),
+});
+
+export const { useGetMessagesQuery, useAddMessageMutation, useEditMessageMutation, useDeleteMessageMutation } = messagesApi;
